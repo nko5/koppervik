9ed6643d03aa74b9475066ae54bff854f72efb0e
David Wolfe <david.p.wolfe@gmail.com>
2015-11-06 20:16:51 -0800

    some more dependencies

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var app = express();
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
 
 //Create a static file server
 app.configure(function() {
   app.use(express.static(__dirname + '/public'));
+});
+
+function handler (req, res) {
+	fs.readFile(__dirname + '/public/index.html', function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			res.end('Error loading index.html');
+		} else {
+			res.writeHead(200);
+			res.end(data);
+		}
+	});
+}
+
+io.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
 });
 
 //Get the dummy data
