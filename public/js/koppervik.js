88faa287f8e08e3e07dbd0c239dd29eee5d55c09
David Wolfe <david.p.wolfe@gmail.com>
2015-11-08 14:22:53 -0800

    just phaser

--- public/js/koppervik.js
+++ public/js/koppervik.js
@@ -7,3 +7,27 @@ socket.on('message', function (data) {
 socket.on('close', function () {
 	console.log('closed');
 });
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gw', {
+	preload: preload,
+	create:  create,
+	update:  update,
+	render:  render,
+});
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
+
+function render() {
+
+}
+
